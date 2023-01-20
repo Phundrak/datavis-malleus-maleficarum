02ce6c665bab2525d4f6df7c8e9fc45f09a2afef
Lucien Cartier-Tilet <lucien@phundrak.com>
2023-01-20 12:01:26 +0100

    init commit
    Initial state inspired by
    https://github.com/toxtli/futuristic-3d-spherical-news-visualization-three-js

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container"></div>
+  <div id="info"></div>
+  <div id="menu" ref="menu">
+    <button id="table">table</button>
+    <button id="sphere">sphere</button>
+    <button id="helix">helix</button>
+    <button id="grid">grid</button>
+    <button id="fulltext">full text</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const scripts = ["/js/data.js", "/js/index.js"];
+
+const menu = ref(null);
+
+onMounted(() => {
+  /* console.debug("Hello");
+   * console.debug(menu.value); */
+  let script = document.createElement("script");
+  script.setAttribute("type", "text/javascript");
+  script.text = `(function () {
+     window.SIG_EXT = {};
+   })();`;
+  menu.value.parentElement.parentElement.appendChild(script);
+  scripts.forEach((src) => {
+    let script = document.createElement("script");
+    script.setAttribute("src", src);
+    menu.value.parentElement.parentElement.append(script);
+  });
+});
+</script>
+
+<style scoped></style>
